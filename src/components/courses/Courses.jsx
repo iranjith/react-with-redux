5fcaa1b002e602e3fb7ab22bb1ea6ca485592308
Ranjith.Kumar <theranjith@gmail.com>
2024-08-20 23:04:31 +0530

    setting up action and reducers

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const Courses = () => {
-  return <div>Courses</div>;
+  const [courseTitle, setCourseTitle] = useState({ course: { title: "" } });
+
+  function handleChange(event) {
+    setCourseTitle({ ...courseTitle, course: { title: event.target.value } });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(courseTitle.course.title);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={courseTitle.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    </>
+  );
 };
 
 export default Courses;
